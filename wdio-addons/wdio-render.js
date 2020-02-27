286277802242ee747a10f101e5ccd4cbb63f7f38
lehtint6 <teemu.t.lehtinen@aalto.fi>
2020-02-27 15:52:47 +0200

    Fix paths and rendering on mocha tests

--- wdio-addons/wdio-render.js
+++ wdio-addons/wdio-render.js
@@ -1,4 +1,4 @@
-/* global module, require, __dirname, console */
+/* global module, require, process, __dirname, console */
 /* jshint globalstrict: true */
 'use strict';
 
@@ -6,7 +6,7 @@ const fs = require('fs');
 const nj = require('nunjucks');
 const baseDir = __dirname;
 
-const res = JSON.parse(fs.readFileSync(__dirname + '/wdio-0-0-json-reporter.log', 'utf8'));
+const res = JSON.parse(fs.readFileSync(__dirname + '/' + process.argv[2], 'utf8'));
 
 nj.configure(__dirname, { autoescape: true });
 console.log(nj.render('wdio-render.html', res));
