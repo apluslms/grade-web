d63032117d52a57e7bddbf3177ce504aa0299b32
lehtint6 <teemu.t.lehtinen@aalto.fi>
2020-02-27 10:49:54 +0200

    Add mocha and some static js checks

--- README.md
+++ README.md
@@ -3,17 +3,36 @@ Grading environment for web exercises.
 Tags
 ----
 
-Images are tagged with grading-base versions in format `<grading-base>`.
-Version tag can also include `uN` meaning _update N_ where N is an increasing number.
-
+The last part of a tag, such as `-3.0` matches the extended grading-base:tag.
 There is also few additional versions of the image:
 
- * `wdio-*` includes nodejs, google chrome and wdio on top of the base
+* `wdio-*` includes nodejs, chrome and wdio on top of the base
 
 
 Utility commands
 ----------------
 
-In addition to [grading-base](https://github.com/apluslms/grading-base), this container provides following utilities.
+In addition to [grading-base](https://github.com/apluslms/grading-base),
+this container provides following utilities.
 
-TODO
+* `wdio/run-wdio-tests`
+
+  Runs an http server in the working directory. Then, executes [wdio](https://webdriver.io/)
+  tests from `/exercise/test*.js` that the exercise must provide. The results are
+  rendered in HTML and captured for feedback & points.
+
+* `wdio/run-mocha-tests`
+
+  Executes [mocha](https://mochajs.org) tests from `/submission/test*.js`
+  (note that you may have to link the necessary tests from /exercise/ before).
+  The results are rendered in HTML and captured for feedback & points.
+
+* `import html_checks`
+
+  Import in python3 code. See the code for common static checks related to HTML, CSS and JS.
+
+* `python3 -m html_checks cmd arguments...`
+
+  Simple sanity checks can be run from command line and `sys.exit == 0` communicates success.
+
+  * `js_parse file_name [function function_name]`
--- python/html_checks.py
+++ python/html_checks.py
@@ -1,3 +1,78 @@
 #!/bin/#!/usr/bin/env python3
+import pyjsparser
 
-# Here shall be common checks for HTML files
+def js_parse(text, description_of_parse_location):
+    try:
+        js = pyjsparser.parse(text)
+        if js['type'] != 'Program':
+            return (
+                False, None,
+                'Unexpectedly JavaScript in {} could not be parsed as a "Program"-entity. '
+                'Report to course staff for guidance.'.format(description_of_parse_location)
+            )
+        body = [s for s in js['body'] if s['type'] != 'EmptyStatement']
+        if len(body) == 0:
+            return (
+                False, None,
+                'Empty JavaScript-code in {}.'.format(description_of_parse_location)
+            )
+        return (
+            True, body,
+            'Validated JavaScript-code in {}.'.format(description_of_parse_location)
+        )
+    except pyjsparser.pyjsparserdata.JsSyntaxError as e:
+        return (
+            False, None,
+            'Encountered syntax error while parsing the JavaScript-code in {}. '
+            'Note, that programming languages are picky and you need to write the commands precisely. '
+            'You should test your solution in browser and check that no errors appear in console panel. '
+            'Below the raw output from the parser program is presented:<ul><li>{}</li></ul>'.format(
+                description_of_parse_location,
+                str(e)
+            )
+        )
+
+def js_has_function(js, function_name):
+    for s in js:
+        if s['type'] == 'FunctionDeclaration' and s['id']['type'] == 'Identifier' and s['id']['name'] == function_name:
+            return (
+                True, s,
+                'Found the declaration of function "{}".'.format(function_name)
+            )
+    return (
+        False, None,
+        'Could not find the declaration of function "{}".'.format(function_name)
+    )
+
+# Command line interface:
+
+OK_LI = '<li><span class="text-success">✔</span> {}</li>'
+ERROR_LI = '<li><span class="text-danger">⨯</span> {}</li>'
+def li_msg(success, message):
+    print((OK_LI if success else ERROR_LI).format(message))
+
+def read_file(file_name):
+    with open(file_name, 'r') as fp:
+        return fp.read()
+
+def main(cmd, *arg):
+    ok = True
+    print('<ul>')
+    if cmd == 'js_parse' and len(arg) > 0:
+        ok, js, msg = js_parse(read_file(arg[0]), arg[0])
+        li_msg(ok, msg)
+        if ok and len(arg) > 2:
+            if arg[1] == 'function':
+                ok, dec, msg = js_has_function(js, arg[2])
+                li_msg(ok, msg)
+    print('</ul>')
+    return ok
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 1:
+        print('Usage: cmd arguments..')
+        print('  js_parse file_name [function function_name]')
+        sys.exit(0)
+    ok = main(sys.argv[0], *sys.argv[1:])
+    sys.exit(0 if ok else 1)
