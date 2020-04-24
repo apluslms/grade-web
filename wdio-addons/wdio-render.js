e35121d204fc11c806b68f82039e138efddf666e
lehtint6 <teemu.t.lehtinen@aalto.fi>
2020-04-24 17:34:35 +0300

    Add node-static & cookie, Handle missing test json

--- wdio-addons/wdio-render.js
+++ wdio-addons/wdio-render.js
@@ -6,59 +6,69 @@ const fs = require('fs');
 const nj = require('nunjucks');
 const baseDir = __dirname;
 
-const res = JSON.parse(fs.readFileSync(__dirname + '/' + process.argv[2], 'utf8'));
-
-nj.configure(__dirname, { autoescape: true });
-console.log(nj.render('wdio-render.html', res));
-
-const passes = (test) => (
-  test.error === undefined
-  && (test.result === undefined || test.result != 'failed')
-);
-
-const pointsRegex = /.*\((\d+)p?\)$/i;
-const getPoints = (item) => {
-  let result = pointsRegex.exec(item.name || item.title);
-  return result ? parseInt(result[1]) : undefined;
-}
-
+let path = baseDir + '/' + process.argv[2];
+let logPath = process.argv[3] ? (baseDir + '/' + process.argv[3]) : undefined;
 let points = 0;
 let maxPoints = 0;
 
-res.suites.forEach((suite) => {
-  let suitePoints = getPoints(suite);
-  let suitePassed = true;
-  let testCount = 0;
-  let testPassedCount = 0;
-  let testPoints = 0;
-  let testMaxPoints = 0;
+if (fs.existsSync(path)) {
+  const res = JSON.parse(fs.readFileSync(path, 'utf8'));
 
-  suite.tests.forEach((test) => {
-    let p = getPoints(test) || 0;
-    testCount += 1;
-    testMaxPoints += p;
-    if (passes(test)) {
-      testPassedCount += 1;
-      testPoints += p;
+  nj.configure(__dirname, { autoescape: true });
+  console.log(nj.render('wdio-render.html', res));
+
+  const passes = (test) => (
+    test.error === undefined
+    && (test.result === undefined || test.result != 'failed')
+  );
+
+  const pointsRegex = /.*\((\d+)p?\)$/i;
+  const getPoints = (item) => {
+    let result = pointsRegex.exec(item.name || item.title);
+    return result ? parseInt(result[1]) : undefined;
+  }
+
+  res.suites.forEach((suite) => {
+    let suitePoints = getPoints(suite);
+    let suitePassed = true;
+    let testCount = 0;
+    let testPassedCount = 0;
+    let testPoints = 0;
+    let testMaxPoints = 0;
+
+    suite.tests.forEach((test) => {
+      let p = getPoints(test) || 0;
+      testCount += 1;
+      testMaxPoints += p;
+      if (passes(test)) {
+        testPassedCount += 1;
+        testPoints += p;
+      } else {
+        suitePassed = false;
+      }
+    });
+
+    if (suitePoints > 0) {
+      maxPoints += suitePoints;
+      if (testMaxPoints > 0) {
+        points += Math.min(testPoints, suitePoints);
+      } else {
+        points += suitePassed ? suitePoints : 0;
+      }
+    } else if (testMaxPoints > 0) {
+      maxPoints += testMaxPoints;
+      points += testPoints;
     } else {
-      suitePassed = false;
+      maxPoints += testCount;
+      points += testPassedCount;
     }
   });
-
-  if (suitePoints > 0) {
-    maxPoints += suitePoints;
-    if (testMaxPoints > 0) {
-      points += Math.min(testPoints, suitePoints);
-    } else {
-      points += suitePassed ? suitePoints : 0;
-    }
-  } else if (testMaxPoints > 0) {
-    maxPoints += testMaxPoints;
-    points += testPoints;
-  } else {
-    maxPoints += testCount;
-    points += testPassedCount;
-  }
-});
+} else {
+  console.log('<p>Unexpected error while running tests!</p>');
+  const logText = (logPath && fs.existsSync(logPath)) ? fs.readFileSync(logPath, 'utf8') : '';
+  console.log('<pre>' + logText + '</pre>');
+  points = 0;
+  maxPoints = 1;
+}
 
 console.log(`\nTotalPoints: ${points}\nMaxPoints: ${maxPoints}\n`);
